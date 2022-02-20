42848e45b99158dfb21085c0ada94eb027f8863a
Sakthivikash <sakthivikash70@gmail.com>
2022-02-20 08:37:04 +0530

    Complited

---
+++ src/Free.js
@@ -0,0 +1,32 @@
+export function Free() {
+
+  return (
+    <div class="col-lg-4">
+      <div class="card mb-5 mb-lg-0">
+        <div class="Cardbody">
+          <h5 class="card-title text-muted text-uppercase text-center">Free</h5>
+          <h6 class="card-price text-center">$0<span class="period">/month</span></h6>
+          <hr />
+          <ul class="fa-ul">
+            <li><span class="fa-li"><i class="fa fa-check"></i></span>Single User</li>
+            <li><span class="fa-li"><i class="fa fa-check"></i></span>5GB Storage</li>
+            <li><span class="fa-li"><i class="fa fa-check"></i></span>Unlimited Public Projects</li>
+            <li><span class="fa-li"><i class="fa fa-check"></i></span>Community Access</li>
+            <li class="text-muted"><span class="fa-li"><i class="fa fa-times" id='X'></i></span>Unlimited
+              Private Projects</li>
+            <li class="text-muted"><span class="fa-li"><i class="fa fa-times" id='X'></i></span>Dedicated
+              Phone Support</li>
+            <li class="text-muted"><span class="fa-li"><i class="fa fa-times" id='X'></i></span>Free Subdomain
+            </li>
+            <li class="text-muted"><span class="fa-li"><i class="fa fa-times" id='X'></i></span>Monthly Status
+              Reports</li>
+          </ul>
+          <div class="d-grid">
+            <a href="#" class="btn btn-primary text-uppercase">BUTTON</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+;
